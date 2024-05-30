6aa0ee19f5270b1b1bd9265e9877058e515cd548
HeitorRaimundoPrado <heitorrdpp@gmail.com>
2024-05-30 01:00:31 -0300

    feat: setup solidjs and axum server

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    routing::get_service,
+    Router
+};
+
+use tower_http::services::ServeDir;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+
+    let service = ServeDir::new("src/static");
+
+    
+    let app = Router::new()
+        .route("/api/hello/world", get(|| async { "Hello, World!" }))
+        .fallback_service(get_service(service));
+
+    
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }
